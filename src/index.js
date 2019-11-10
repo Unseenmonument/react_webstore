63dce6b308de32ad46a5cdd0e0d9d114d002b64d
Kerron S. Streater <kerronstreater@gmail.com>
2019-11-10 01:04:15 -0500

    4/6ths through turorial

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import {BrowserRouter as Router} from 'react-router-dom';
+import {ProductProvider} from "./context";
+import './index.css';
+// import * as serviceWorker from './serviceWorker';
+
+ReactDOM.render(
+  <ProductProvider>
+    <Router>
+      <App />
+    </Router>
+  </ProductProvider>,
+  document.getElementById('root')
+);
